569fa598aff0a12ce44d106d42faeb95a69fa7f2
Pablo <pablo.trescoli@yahoo.es>
2020-04-06 00:20:29 +0100

    - month viewer = no go button - I'm going to "fork" the project or whatever - and make the calendarview groupview views for header and shit not private so I can modify the colour of background

--- app/src/main/java/com/tatoe/mydigicoach/ui/calendar/MonthViewer.kt
+++ app/src/main/java/com/tatoe/mydigicoach/ui/calendar/MonthViewer.kt
@@ -2,8 +2,10 @@ package com.tatoe.mydigicoach.ui.calendar
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.CalendarView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.startActivity
 import com.tatoe.mydigicoach.R
 import com.tatoe.mydigicoach.entity.Day
 import kotlinx.android.synthetic.main.activity_month_viewer.*
@@ -12,7 +14,7 @@ import timber.log.Timber
 class MonthViewer : AppCompatActivity() {
 
     companion object {
-        const val DAY_ID_KEY="day_id"
+        const val DAY_ID_KEY = "day_id"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,22 +23,19 @@ class MonthViewer : AppCompatActivity() {
         title = "Exercise Result"
 
         var calendar = calendarView
-        var button = goToDayBtn
+//        var button = goToDayBtn
 
-        button.setOnClickListener {
-            Timber.d("OIOI ${calendar.selectedDate}")
-            if (calendar.selectedDate!=null) {
-                var dayId= Day.intDatetoDayId(calendar.selectedDate!!.day,calendar.selectedDate!!.month,calendar.selectedDate!!.year)
+        calendar.setOnDateChangedListener { _, date, selected ->
+            if (selected) {
+                var dayId = Day.intDatetoDayId(date.day, date.month, date.year)
                 Timber.d("OIOI 2 $dayId")
                 var intent = Intent(this, WeekViewer::class.java)
                 intent.putExtra(DAY_ID_KEY, dayId)
                 startActivity(intent)
-            } else {
-                Toast.makeText(this,"Select a date first",Toast.LENGTH_SHORT).show()
             }
-
         }
 
     }
+
 
 }
