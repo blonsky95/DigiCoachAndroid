f6425b6d6a4897d9f585c28fbac93506e090c3db
Pablo <pablo.trescoli@yahoo.es>
2020-05-01 16:20:36 +0100

    - added a button to add stub data from homescreen menu

--- app/src/main/java/com/tatoe/mydigicoach/viewmodels/LoginSignUpViewModel.kt
+++ app/src/main/java/com/tatoe/mydigicoach/viewmodels/LoginSignUpViewModel.kt
@@ -15,6 +15,8 @@ import com.google.firebase.firestore.FirebaseFirestore
 import com.tatoe.mydigicoach.AppRepository
 import com.tatoe.mydigicoach.database.AppRoomDatabase
 import com.tatoe.mydigicoach.entity.Day
+import com.tatoe.mydigicoach.entity.Exercise
+import com.tatoe.mydigicoach.network.MyCustomFirestoreExercise
 import com.tatoe.mydigicoach.ui.util.DataHolder
 import kotlinx.coroutines.launch
 import timber.log.Timber
@@ -165,5 +167,65 @@ class LoginSignUpViewModel(var application: Application, var db: FirebaseFiresto
         }
 
         DataHolder.userEmail = auth.currentUser!!.email!!
+    }
+
+    fun addBunchOfStubStoreExercises() {
+        val olympicLiftingExes = arrayListOf<Exercise>()
+        olympicLiftingExes.add(Exercise("Snatch","Lift bar from ground, hit at hips and gain control over bar either as power snatch or normal snatch (full depth)"))
+        olympicLiftingExes.add(Exercise("Hang snatch","Lift bar from hips, gain control over bar either as power snatch or normal snatch (full depth)"))
+        olympicLiftingExes.add(Exercise("Clean","Lift bar from ground, hit at hips and gain control over bar either as power clean or normal clean (full depth)"))
+        olympicLiftingExes.add(Exercise("Hang Clean","Lift bar from hips, gain control over bar either as power snatch or normal snatch (full depth)"))
+        olympicLiftingExes.add(Exercise("Clean & jerk","Lift bar from ground, hit at hips and gain control over bar"))
+
+        val longDistRuns = arrayListOf<Exercise>()
+        longDistRuns.add(Exercise("3km","3km optimal time 11:00"))
+        longDistRuns.add(Exercise("5km","5km optimal time 20:00"))
+        longDistRuns.add(Exercise("10km","10km optimal time 45:00"))
+        longDistRuns.add(Exercise("Half-marathon","21.098km"))
+        longDistRuns.add(Exercise("Marathon","42.196km lol have fun"))
+
+        val sprints = arrayListOf<Exercise>()
+        sprints.add(Exercise("30m","between 5 and 8 reps, recovery of 3/4 minutes minimum"))
+        sprints.add(Exercise("100m","2 sets of 10 runs at 85% intensity, with 1-2 min recovery, full rest between sets"))
+        sprints.add(Exercise("Lactic pyramid","100-120-150-180-150-120-100 recovery of 5-6 min"))
+
+        val powerlifting = arrayListOf<Exercise>()
+        powerlifting.add(Exercise("Squats","Full depth squats"))
+        powerlifting.add(Exercise("Bench Press","Touch chest for max gains"))
+        powerlifting.add(Exercise("Deadlift","Technique-wise make sure straight back, and engaging posterior chain when starting lift, start with legs not with back"))
+
+        for (exercise in olympicLiftingExes) {
+            db.collection("store_exercises").document("olympic_lifting").collection("exercises").document(exercise.name)
+                .set(MyCustomFirestoreExercise(exercise))
+                .addOnSuccessListener {
+                    Timber.d("DocumentSnapshot successfully written!")
+                }
+                .addOnFailureListener {
+                        e -> Timber.d("Error writing document: $e")
+                }
+        }
+
+        for (exercise in longDistRuns) {
+            db.collection("store_exercises").document("long_distance_running").collection("exercises").document(exercise.name)
+                .set(MyCustomFirestoreExercise(exercise))
+                .addOnSuccessListener { Timber.d("DocumentSnapshot successfully written!") }
+                .addOnFailureListener { e -> Timber.d("Error writing document: $e") }
+        }
+
+        for (exercise in sprints) {
+            db.collection("store_exercises").document("sprinting").collection("exercises").document(exercise.name)
+                .set(MyCustomFirestoreExercise(exercise))
+                .addOnSuccessListener { Timber.d("DocumentSnapshot successfully written!") }
+                .addOnFailureListener { e -> Timber.d("Error writing document: $e") }
+        }
+
+        for (exercise in powerlifting) {
+            db.collection("store_exercises").document("powerlifting").collection("exercises").document(exercise.name)
+                .set(MyCustomFirestoreExercise(exercise))
+                .addOnSuccessListener { Timber.d("DocumentSnapshot successfully written!") }
+                .addOnFailureListener { e -> Timber.d("Error writing document: $e") }
+        }
+
+
     }
 }
